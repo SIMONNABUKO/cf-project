366c99d5b0465c4db48bc8594847ad691e906357
Simon Angatia <simonnabuko@gmail.com>
2020-01-27 07:44:16 +0300

    second commit

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+// var firebaseConfig = {
+//   apiKey: 'AIzaSyCdRVavRpo5G1P6hZkRMj21FjmGM5LDVpk',
+//   authDomain: 'cf-forms.firebaseapp.com',
+//   databaseURL: 'https://cf-forms.firebaseio.com',
+//   projectId: 'cf-forms',
+//   storageBucket: 'cf-forms.appspot.com',
+//   messagingSenderId: '930856768039',
+//   appId: '1:930856768039:web:cc8abe694651113bf1b402',
+//   measurementId: 'G-R3MCCF1M5M'
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+
 new cf.ConversationalForm({
   formEl: document.querySelector('#form'),
   theme: 'blue',
@@ -15,7 +28,11 @@ new cf.ConversationalForm({
   },
   submitCallback: function() {
     this.addRobotChatResponse(
-      'Thank you for your time, we will contact you soon'
+      'Thank you for your details, we will get back to you soon'
     );
+    var formData = this.getFormData();
+    var formDataSerialized = this.getFormData(true);
+    console.log('Formdata:', formData);
+    console.log('Formdata, serialized:', formDataSerialized);
   }
 });
